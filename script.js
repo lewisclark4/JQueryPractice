57be916c521e3ce0b34663fbab232ad098ebf3f4
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-02-10 11:57:09 +0000

    added multiple jQuery functions

--- script.js
+++ script.js
@@ -17,4 +17,31 @@ $(document).ready(function() {
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
    });
+   $("p").on("click", function() {
+        $("p").css('color', 'red');
+   });
+   $("h2").hover(function() {
+        $("h2").css('background-color', 'lightblue');
+   });
+   $("h2").hover(function(){
+        $(this).css('font-size', '1.5em');
+    }, function(){
+        $(this).css('font-size', '1em');
+    });
+   $(".bottom_button").hover(function() {
+        $("#container").css('background-color', 'black');
+   }, function() {
+        $("#container").css('background-color', 'grey');
+   });
+
+   $(".bottom_button").click(function() {
+        $(this).siblings('p').slideToggle();
+   });
+
+    $(".bottom_button").hover(function() {
+        $(this).fadeTo(100, 0.5);
+   }, function() {
+        $(this).fadeTo(100, 1);
+   });
 }); 
+
